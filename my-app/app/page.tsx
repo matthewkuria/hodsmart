06130ee440e66bc020df3e94e392a547143189dc
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 19:01:48 +0300

    add logo to our the page.tsx in the app

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { lusitana } from '@/app/ui/fonts';
+import HodSmartLogo from "./ui/HodSmartLogo";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="">
-        
+        <HodSmartLogo />
       </div>
     </main>
   );
---
+++ my-app/app/ui/HodSmartLogo.tsx
@@ -0,0 +1,8 @@
+import { lusitana } from "./fonts"
+export default function HodSmartLogo(){
+    return(
+        <h1 className={`${lusitana.className}`}>
+            HOD Smart
+        </h1>
+    )
+}
