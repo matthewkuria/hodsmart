c93bace4ba66975e8bef602e9814899d27f31ef0
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 22:26:45 +0300

    build the form

--- my-app/app/ui/login-form.tsx
+++ my-app/app/ui/login-form.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 import { lusitana } from "./fonts"
 
 
@@ -8,15 +10,30 @@ const formSchema = z.object({
     password: z.string().min(8)
   })
 export default function LoginForm(){
-    return(
-        <form >
-            <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-            <h1 className={`${lusitana.className} mb-3 text-2xl`}>
+    return(     
+         <Form {...form}>
+         <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
                  Please log in to continue.
             </h1>
-
-            </div>
-
-        </form>
+           <FormField
+             control={form.control}
+             name="username"
+             render={({ field }) => (
+               <FormItem>
+                 <FormLabel>Username</FormLabel>
+                 <FormControl>
+                   <Input placeholder="shadcn" {...field} />
+                 </FormControl>
+                 <FormDescription>
+                   This is your public display name.
+                 </FormDescription>
+                 <FormMessage />
+               </FormItem>
+             )}
+           />
+           <Button type="submit">Submit</Button>
+         </form>
+       </Form>
     )
 }
