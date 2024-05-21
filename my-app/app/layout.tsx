710a368f7b29c389d9464077d54046ceacf24704
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 18:35:43 +0300

    import inter font from google fonts

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import '@/app/ui/global.css';
-
-const inter = Inter({ subsets: ["latin"] });
-
+import { inter } from '@/app/ui/fonts';
 export const metadata: Metadata = {
   title: "HOD smart",
   description: "Become smart doing your lesson allocations",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
