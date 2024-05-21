9a8ea318cf81f8a57a928873d5c590d4001feaac
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 18:57:52 +0300

    cahnge flex properties

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,8 +3,10 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1 className={`${lusitana.className} font-bold`}>HOD Smart</h1>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="">
+        
+      </div>
     </main>
   );
 }
