50aa047f766db8345c9575f1cbea629c31943f81
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 19:04:45 +0300

    customise the logo

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,7 +5,7 @@ import HodSmartLogo from "./ui/HodSmartLogo";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="">
+      <div className="flex h-20 shrink-0 items-end rounded-lg p-4 md:h-52 bg-blue-600">
         <HodSmartLogo />
       </div>
     </main>
