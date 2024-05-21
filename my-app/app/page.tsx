7885148fd1756ed759f22ec5a429f3ae2456184d
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 18:54:24 +0300

    add pictures to the public folder

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Dashboard Goes Here</h1>
+     <h1 className={`${lusitana.className} font-bold`}>HOD Smart</h1>
     </main>
   );
 }
--- my-app/app/ui/fonts.ts
+++ my-app/app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Roboto_Mono } from 'next/font/google'
+import { Inter, Roboto_Mono, Lusitana } from 'next/font/google'
  
 export const inter = Inter({
   subsets: ['latin'],
@@ -9,3 +9,8 @@ export const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
   display: 'swap',
 })
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
