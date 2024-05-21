16a94d3513bdc61cf8aca64bb69f27dc36ecccdb
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 19:15:33 +0300

    add images to the page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -8,6 +8,20 @@ export default function Home() {
       <div className="flex h-20 shrink-0 items-end rounded-lg p-4 md:h-52 bg-blue-600">
         <HodSmartLogo />
       </div>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="">
+
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+            <Image
+              src="/smart-teacher-desktop.jfif"
+              width={1000}
+              height={760}
+              alt="Smart Hod image for the desktop"
+             />
+        </div>
+
+      </div>
     </main>
   );
 }
--- my-app/app/ui/HodSmartLogo.tsx
+++ my-app/app/ui/HodSmartLogo.tsx
@@ -1,7 +1,7 @@
 import { lusitana } from "./fonts"
 export default function HodSmartLogo(){
     return(
-        <h1 className={`${lusitana.className}`}>
+        <h1 className={`${lusitana.className} font-bold text-white text-3xl`}>
             HOD Smart
         </h1>
     )
