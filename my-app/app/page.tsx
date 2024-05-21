306aaf4cacfbfee1b8492d876bc1c1febecd4e5e
matthewkuria <mathewkwachira@gmail.com>
2024-05-21 19:34:29 +0300

    add heroicons to the project

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
             Welcome to HOD smart platform
            </h1>
            <p className="">
-           This site is designed to facilitate smooth error-proof way of capturing lesson allocations for HODs during teachers meetings.
+           This site is designed to facilitate smooth error-proof way of capturing lesson allocations for your teachers by the HODs during departmental meetings.
            </p>
            <p className="">
-           We know that it takes time to share your allocate lessons and capture the allocations, so we'd like to make it worth your while by offering you a smart platform to do it paperlessly.
+           We know that it takes time to share  lessons and capture the allocations, so we'd like to make it worth your while by offering you a smart platform to do it paperlessly.
            </p>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
