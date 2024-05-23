0fe969e17c5061ab1d3711602fb2e9402042e654
matthewkuria <mathewkwachira@gmail.com>
2024-05-23 22:12:41 +0300

    format cells in the columns

--- app/dashboard/teachers/page.tsx
+++ app/dashboard/teachers/page.tsx
@@ -1,3 +1,31 @@
-export default function Page() {
-    return <p>Teachers Page</p>;
-  }
+import { Payment, columns } from "./columns"
+import { DataTable } from "@/app/ui/data-table"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 300,
+      status: "processing",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
