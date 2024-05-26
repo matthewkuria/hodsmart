3e4ceb87fff89a1263af8a36fca41a420475649a
matthewkuria <mathewkwachira@gmail.com>
2024-05-26 22:31:39 +0300

    import button and create the form

--- app/ui/teachers/create-teacher-form.tsx
+++ app/ui/teachers/create-teacher-form.tsx
@@ -1,6 +1,18 @@
 "use client"
-
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
     tscNumber:  z.string().min(2).max(50),
@@ -8,3 +20,18 @@ const formSchema = z.object({
     subjects:  z.string().min(2).max(50),
     
 })
+export function CreateTeacherForm(){
+      // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      tscNumber: "",
+    },
+  })
+   // 2. Define a submit handler.
+   function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+}
