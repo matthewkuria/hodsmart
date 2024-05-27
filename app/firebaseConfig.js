8c9c73d797f43ae011890fb7034dc1e118bb1834
matthewkuria <mathewkwachira@gmail.com>
2024-05-27 21:36:46 +0300

    Connect the app to firebase db

---
+++ app/firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAflyHGw5qxhyEDWZ-UC_WJ18nUkCof1go",
+  authDomain: "hodsmart-a1108.firebaseapp.com",
+  projectId: "hodsmart-a1108",
+  storageBucket: "hodsmart-a1108.appspot.com",
+  messagingSenderId: "1031926842287",
+  appId: "1:1031926842287:web:0da74af6d7c7c70484e55a"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
