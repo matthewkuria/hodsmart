ef176ae281d943919a49de786cebaa537559721e
matthewkuria <mathewkwachira@gmail.com>
2024-05-30 20:52:55 +0300

    add search component to the dashboard

---
+++ app/ui/dashboard/home/search-component.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { Input } from "@/components/ui/input"
+import HodSmartLogo from "../../HodSmartLogo"
+export default function SearchComponent() {
+    <main className="">
+        <div className="">
+            <HodSmartLogo />
+
+        </div>        
+    </main>
+}
