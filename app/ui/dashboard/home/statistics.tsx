75a921686620937871d95d285596e74956a98172
matthewkuria <mathewkwachira@gmail.com>
2024-05-30 23:35:25 +0300

    add margin all round

--- app/ui/dashboard/home/statistics.tsx
+++ app/ui/dashboard/home/statistics.tsx
@@ -12,19 +12,20 @@ import {
 
 export default function DashboardStatistics() {
     return (
-        <main className="bg-slate-50 h-56 mt-4">
-           <Card className="bg-white flex flex-col items-center rounded-lg border w-[150px] h-[150px] p-4 hover:border-blue-500 shadow-lg">
+        <main className="bg-slate-100 h-56 mt-4">
+           <Card className="bg-white  items-center rounded-lg  w-[150px] h-[150px] m-4 p-4 border-4 border-blue-400 hover:border-blue-500 hover:shadow-lg">
                 <CardHeader>
                     <CardTitle>Teachers</CardTitle>                   
                 </CardHeader>
-                <CardContent>
+                <CardContent className=" flex flex-col mx-auto items-center">
                     <Image
                         src="/teacher-icon.png"
                         alt=""
                         width={120}
                         height={150}
+                        className="justify-center"
                     />
-                    <h3 className="text-blue-500">66</h3>
+                    <h3 className="font-bold">66</h3>
                 </CardContent>                               
             </Card>
         </main>
