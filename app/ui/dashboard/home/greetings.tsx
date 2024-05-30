5d8a33bbf61021e84a7bc1fd92fd9d853686ff51
matthewkuria <mathewkwachira@gmail.com>
2024-05-30 22:30:08 +0300

    add images for the desktop and mobile designs

--- app/ui/dashboard/home/greetings.tsx
+++ app/ui/dashboard/home/greetings.tsx
@@ -1,13 +1,29 @@
 "use client"
-
+import Image from "next/image"
 export default function GreetingsComponent() {
     return (
         <main className=" flex items-center justify-between bg-gradient-to-r from-blue-500 h-40 rounded-lg">
             <div className="flex items-center">
-                <p className="text-white text-xl">Welcome back,HOD!</p>
+                <h1 className="text-white text-xl">Welcome back,HOD!</h1>
+                <p className=""></p>
             </div>
             <div className="">
-              <p>image here</p>
+                {/* Image for the desktop design */}
+                <Image
+                    src="/teacher-image-desktop.png"
+                    alt="Teacher image for desktop"
+                    className="hidden md:block rounded-sm mr-10"
+                    height={100}
+                    width={200}
+                />
+                {/* Image for the mobile design */}
+                <Image
+                    src="/teacher-image-mobile.png"
+                    alt="Teacher image for desktop"
+                    className="md:hidden block rounded-sm mr-10"
+                    height={100}
+                    width={200}
+                />
             </div>
         </main>
     )
