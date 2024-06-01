c98c2b909aaa8207f4ea78ec1249260d8b43db20
matthewkuria <mathewkwachira@gmail.com>
2024-06-01 21:47:17 +0300

    Add get auth to firebaseconfig file

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,6 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAflyHGw5qxhyEDWZ-UC_WJ18nUkCof1go",
@@ -13,4 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 export { db };
+export { auth };
