4be5a58e8fc76ad54404e1f5f86f614fc5cd3175
matthewkuria <mathewkwachira@gmail.com>
2024-06-03 21:06:14 +0300

    Create formSchema

--- app/ui/subjects/create-subjects.tsx
+++ app/ui/subjects/create-subjects.tsx
@@ -1,7 +1,25 @@
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
+
+const formSchema = z.object({
+    subCode: z.number(),
+    subName: z.string().min(3, {
+        message: "Subject name must contain more than 3 characters "
+    })
+})
 export default function AddSubjectsForm() {
+     const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      subCode: "",
+      subName: "",
+    },
+  })
     return (
-        <Form>
+        <Form {...form}>
+
             
         </Form>
     )
