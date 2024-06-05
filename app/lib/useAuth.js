64b081f316fd7a6d1814ff1955a8e560c331943e
matthewkuria <mathewkwachira@gmail.com>
2024-06-05 22:59:14 +0300

    Fix the sign up form

--- app/lib/useAuth.js
+++ app/lib/useAuth.js
@@ -3,7 +3,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 
 const useAuth = () => {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
