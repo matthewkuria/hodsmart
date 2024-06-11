6ec5d4aacf78bd24880bbc4e9a195a25fa6efce2
matthewkuria <mathewkwachira@gmail.com>
2024-06-11 21:34:41 +0300

    render allocations card

--- app/dashboard/allocations/page.tsx
+++ app/dashboard/allocations/page.tsx
@@ -1,3 +1,5 @@
 export default function Page() {
-    return <p>Lesson Allocation Page</p>;
+  return (
+      <AddAllocationCard />
+    )
   }
--- app/ui/allocations/add-allocations.tsx
+++ app/ui/allocations/add-allocations.tsx
@@ -1,12 +1,50 @@
-import { Form } from "@/components/ui/form";
+"use client"
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod"
 
-export default function addAllocationCard() {
-    const form = useForm()
+const formSchema = z.object({
+  teachername: z.string().min(2, {
+    message: "Teacher's name must be at least 3 characters.",
+  }),
+})
+export default function AddAllocationCard() {
+    const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      teachername: "",
+    },
+    })
+     function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
     return (
      <main>
-        <Form >
-            
+        <Form {...form} >
+            <form action="">
+                <FormField
+                    control={form.control}
+                    name="teachername"
+                    render={({ field }) => (
+                        <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input placeholder="Teacher's Name" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+                    <Button type="submit">Allocate</Button>
+            </form>
         </Form>
      </main>
  )
