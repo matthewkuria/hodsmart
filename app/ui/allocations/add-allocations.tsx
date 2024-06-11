059e13201b4ebd190076e4ac2ddccfca93f813f4
matthewkuria <mathewkwachira@gmail.com>
2024-06-11 23:50:54 +0300

    Fix: Allocations form

--- app/ui/allocations/add-allocations.tsx
+++ app/ui/allocations/add-allocations.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { db } from "../../firebaseConfig"
 import { collection, addDoc } from "firebase/firestore";
+import useFetchSubjectData from "@/app/lib/subject-data";
 import schData from "@/app/lib/sch-classes"
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -16,18 +17,42 @@ const formSchema = z.object({
   teacherName: z.string().min(2, {
     message: "Teacher's name must be at least 3 characters.",
   }),
-    items: z.array(z.string()).refine((value) => value.some((item) => item), {
+    subjects: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: "You have to select at least one class.",
+  }),
+    classesTaught: z.array(z.string()).refine((value) => value.some((item) => item), {
     message: "You have to select at least one class.",
   }),
   numberOfLessons: z.string()
 })
+// Function to add a document to a specified collection
+const addAllocation = async (collectionName:any, documentData:any) => {
+  try {
+    const docRef = await addDoc(collection(db, collectionName), documentData);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+};
+
 export default function AddAllocationCard() {
+  const { data, loading } = useFetchSubjectData(); 
   const [formData, setFormData] = useState({
       teacherName: "",
-      fullName: "",
+      numberOflessons: "",
       subjects: "",
-      gender:""
+      classesTaught:""
   })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      teacherName: "",
+      classesTaught: ["1b"],
+      subjects:["Eng"],
+      numberOfLessons: "",
+
+    },
+    })
    // create handleChange function to collect data when there is a change
   const handleChange = (e: any) => {    
     const { name, value } = e.target;
@@ -35,38 +60,33 @@ export default function AddAllocationCard() {
       ...prevFormData,
       [name]: value
     }));
+    
   };
     const handleSubmit = async (e:any) => {
     e.preventDefault();
     const documentData = {
        teacherName: formData.teacherName,
-      fullName: formData.fullName,
+      classesTaught: formData.classesTaught,
       subjects: formData.subjects,
-      gender: formData.gender
+      numberOflessons: formData.numberOflessons
     };
-    const collectionName = 'teachers';  // Specify your collection name here
-    //  await addTeacher(collectionName, documentData);    
-      alert("Teacher added successfully!")    
+    const collectionName = 'allocations';  // Specify your collection name here
+     await addAllocation(collectionName, documentData);    
+      alert("Allocation done successfully!")    
     // Reset the form
     setFormData({
       teacherName: "",
-      fullName: "",
+      classesTaught: "",
       subjects: "",
-      gender:"male"
+      numberOflessons:""
     });
   };
-    const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      teacherName: "",
-      items:[]
-    },
-    })
+    
    
     return (
      <main>
         <Form {...form} >
-            <form onSubmit={form.handleSubmit(handleSubmit)} className="md:flex">
+            <form onSubmit={handleSubmit} className="md:flex">
                 <FormField
                     control={form.control}
                     name="teacherName"
@@ -75,17 +95,69 @@ export default function AddAllocationCard() {
                         <FormLabel>Teacher</FormLabel>
                         <FormControl>
                           <Input placeholder="Teacher's Name"
-                            name="teachername"
-                            value={formData.value}
-                            onChange={ handleChange}/>
+                            name="teacherName"
+                            id="teacherName"
+                            value={formData.teacherName}
+                            onChange={handleChange}
+                             required
+                          />
+                            
                         </FormControl>                        
                         <FormMessage />
                         </FormItem>
                     )}
-                    />
+            />
+            <FormField
+          control={form.control}
+          name="subjects"
+          render={() => (
+            <FormItem>
+              <div className="mb-4">
+                <FormLabel className="text-base">Subjects</FormLabel>
+                <FormDescription>
+                  Select the Subjects taught.
+                </FormDescription>
+              </div>
+              {data.map((subject) => (
+                <FormField
+                  key={subject.id}
+                  control={form.control}
+                  name="subjects"
+                  render={({ field }) => {
+                    return (
+                      <FormItem
+                        key={subject.id}
+                        className="flex flex-row items-start space-x-3 space-y-0"
+                      >
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value?.includes(subject.subCode)}
+                            onCheckedChange={(checked) => {
+                              return checked
+                                ? field.onChange([...field.value, subject.subCode])
+                                : field.onChange(
+                                    field.value?.filter(
+                                      (value) => value !== subject.subCode
+                                    )
+                                  )
+                            }}
+                          />
+                        </FormControl>
+                        <FormLabel className="font-normal">
+                          {subject.subName}
+                        </FormLabel>
+                      </FormItem>
+                    )
+                  }}
+                />
+              ))}
+              <FormMessage />
+            </FormItem>
+          )}
+            />
                 <FormField
           control={form.control}
-          name="items"
+          name="classesTaught"
           render={() => (
             <FormItem>
               <div className="mb-4">
@@ -98,7 +170,7 @@ export default function AddAllocationCard() {
                 <FormField
                   key={item.id}
                   control={form.control}
-                  name="items"
+                  name="classesTaught"
                   render={({ field }) => {
                     return (
                       <FormItem
@@ -132,17 +204,26 @@ export default function AddAllocationCard() {
           )}
             />
             <div className="w-full">
-              <Label htmlFor="gender"></Label>
+              <Label htmlFor="numberOfLessons"></Label>
             <select
-              id="gender"
-              name="gender"
-              value={formData.gender}
+              id="numberOfLessons"
+              name="numberOfLessons"
+              value={formData.numberOflessons}
               onChange={handleChange}
               required
             >     
-            <option value="">--Choose Gender--</option>  
-              <option value="male" >Male</option>
-              <option value="female">Female</option>
+            <option value="0">--The number of Lessons--</option>  
+              <option value="1" >1</option>
+              <option value="2" >2</option>
+              <option value="3" >3</option>
+              <option value="4" >4</option>
+              <option value="5" >5</option>
+              <option value="6" >6</option>
+              <option value="7" >7</option>
+              <option value="8" >8</option>
+              <option value="9" >9</option>
+              <option value="10" >10</option>
+              
             </select>
           </div>
                     <Button type="submit">Allocate</Button>
