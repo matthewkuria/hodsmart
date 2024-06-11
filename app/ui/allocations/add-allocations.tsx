8d66d366c7bff5b631718b36967a0a6143d0fa98
matthewkuria <mathewkwachira@gmail.com>
2024-06-11 21:23:40 +0300

    Add form to the allocations card

---
+++ app/ui/allocations/add-allocations.tsx
@@ -0,0 +1,13 @@
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+
+export default function addAllocationCard() {
+    const form = useForm()
+    return (
+     <main>
+        <Form >
+            
+        </Form>
+     </main>
+ )
+}
