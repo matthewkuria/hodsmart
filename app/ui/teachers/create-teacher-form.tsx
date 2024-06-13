7b16b9a49189717e43a16b4cf6e449d4c08a4d90
matthewkuria <mathewkwachira@gmail.com>
2024-06-13 22:09:09 +0300

    Remove unnecessary imports for the form inputs

--- app/ui/teachers/create-teacher-form.tsx
+++ app/ui/teachers/create-teacher-form.tsx
@@ -8,13 +8,7 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  Form, 
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Label } from "@radix-ui/react-label";
