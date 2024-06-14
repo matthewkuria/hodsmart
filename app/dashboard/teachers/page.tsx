9a841f4e97e341b47673c126ef5bcc893c9e870d
matthewkuria <mathewkwachira@gmail.com>
2024-06-14 23:05:04 +0300

    Protect teachers page withAuth

--- app/dashboard/teachers/page.tsx
+++ app/dashboard/teachers/page.tsx
@@ -1,8 +1,11 @@
+"use client"
+import withAuth from "@/app/lib/withAuth"
 import  DataTable  from "@/app/ui/data-table"
-export default async function Page() {
+ const Page = async () => {
   return (
     <div className="container mx-auto py-10">
       <DataTable  />
     </div>
   )
 }
+export default withAuth(Page);
